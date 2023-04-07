fd88704a3d76d310a84fd263059c088777055844
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 15:56:55 -0300

    created repository Category

---
+++ src/main/java/dev/natao/projectweb/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package dev.natao.projectweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.natao.projectweb.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
