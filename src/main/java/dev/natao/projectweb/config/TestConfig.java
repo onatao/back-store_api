f4ae4b18bb4081c8afbd1569537ecfa5a88a17b3
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 11:59:48 -0300

    adding OrderStatus (TestConfig)

--- src/main/java/dev/natao/projectweb/config/TestConfig.java
+++ src/main/java/dev/natao/projectweb/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import dev.natao.projectweb.entities.Order;
 import dev.natao.projectweb.entities.User;
+import dev.natao.projectweb.entities.enums.OrderStatus;
 import dev.natao.projectweb.repositories.OrderRepository;
 import dev.natao.projectweb.repositories.UserRepository;
 
@@ -23,14 +24,17 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private OrderStatus orderStatus;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Nathan Barros", "nathan@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Arthur Barros", "arthur@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), orderStatus.valueOf(0), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), orderStatus.valueOf(0), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), orderStatus.valueOf(0), u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));	
