7f5e96af73ed32ec994686c96377968090266943
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 10:49:59 -0300

    adjusting lines (unnecessary imports)

--- src/main/java/dev/natao/projectweb/repositories/UserRepository.java
+++ src/main/java/dev/natao/projectweb/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package dev.natao.projectweb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.natao.projectweb.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
