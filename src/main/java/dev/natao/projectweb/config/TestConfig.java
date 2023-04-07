aeac9b8c2bb4845dd2d0b0a545e82aa715450035
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 16:39:36 -0300

    database seeding - Product

--- src/main/java/dev/natao/projectweb/config/TestConfig.java
+++ src/main/java/dev/natao/projectweb/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import dev.natao.projectweb.entities.Category;
 import dev.natao.projectweb.entities.Order;
+import dev.natao.projectweb.entities.Product;
 import dev.natao.projectweb.entities.User;
 import dev.natao.projectweb.entities.enums.OrderStatus;
 import dev.natao.projectweb.repositories.CategoryRepository;
 import dev.natao.projectweb.repositories.OrderRepository;
+import dev.natao.projectweb.repositories.ProductRepository;
 import dev.natao.projectweb.repositories.UserRepository;
 
 @Configuration
@@ -29,11 +31,20 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		User u1 = new User(null, "Nathan Barros", "nathan@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Arthur Barros", "arthur@gmail.com", "977777777", "123456"); 
@@ -45,5 +56,6 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));	
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
