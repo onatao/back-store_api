e24d58a614e291beabc2aa3c2b405542b4c89df8
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 12:12:00 -0300

    fixing TestConfig

--- src/main/java/dev/natao/projectweb/config/TestConfig.java
+++ src/main/java/dev/natao/projectweb/config/TestConfig.java
@@ -24,17 +24,14 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
-	@Autowired
-	private OrderStatus orderStatus;
-	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Nathan Barros", "nathan@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Arthur Barros", "arthur@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), orderStatus.valueOf(0), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), orderStatus.valueOf(0), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), orderStatus.valueOf(0), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));	
