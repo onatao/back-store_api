afbbaa0e81426a0db63a52d5bd61e3f2429116d2
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 10:41:28 -0300

    UserResource findAll improved

--- src/main/java/dev/natao/projectweb/resources/UserResource.java
+++ src/main/java/dev/natao/projectweb/resources/UserResource.java
@@ -1,5 +1,8 @@
 package dev.natao.projectweb.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.natao.projectweb.entities.User;
+import dev.natao.projectweb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new  User();
-		user.setUserName("Nathan");
-		return new ResponseEntity<>(user, HttpStatus.OK);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> responseList= service.findAll();
+		return new ResponseEntity<>(responseList, HttpStatus.OK);
 	}
 
 }
