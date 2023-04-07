c7ecb0620c2466ca340c0f75ce19d851c7d39761
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 10:30:37 -0300

    UserResource and findAll created

---
+++ src/main/java/dev/natao/projectweb/resources/UserResource.java
@@ -0,0 +1,22 @@
+package dev.natao.projectweb.resources;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.natao.projectweb.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new  User();
+		user.setUserName("Nathan");
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+}
