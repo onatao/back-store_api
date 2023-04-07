70d1e086290f8f8c29de7589ae4f16497981fd2a
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 11:21:59 -0300

    OrderService fixed

--- src/main/java/dev/natao/projectweb/services/OrderService.java
+++ src/main/java/dev/natao/projectweb/services/OrderService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import dev.natao.projectweb.entities.Order;
 import dev.natao.projectweb.repositories.OrderRepository;
 
+@Service
 public class OrderService {
 	
 	@Autowired
