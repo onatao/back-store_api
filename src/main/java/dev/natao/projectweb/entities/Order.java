6490d5350f6f46fd51a1b6aedf925a8b6ad2d5a3
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 10:58:59 -0300

    ManyToOne config (Order - User)

--- src/main/java/dev/natao/projectweb/entities/Order.java
+++ src/main/java/dev/natao/projectweb/entities/Order.java
@@ -4,10 +4,16 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -16,6 +22,8 @@ public class Order implements Serializable {
 	private Long orderId;
 	private Instant orderMoment;
 	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
 	private User client;
 
 	public Order(Long orderId, Instant orderMoment, User client) {
