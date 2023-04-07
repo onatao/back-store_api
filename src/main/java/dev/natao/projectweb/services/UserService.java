c0c735b9ef44775e50c14baefd9e630e3fe1d9ee
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-07 10:45:02 -0300

    Service fix

--- src/main/java/dev/natao/projectweb/services/UserService.java
+++ src/main/java/dev/natao/projectweb/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import dev.natao.projectweb.entities.User;
 import dev.natao.projectweb.repositories.UserRepository;
 
+@Service
 public class UserService {
 	
 	@Autowired
