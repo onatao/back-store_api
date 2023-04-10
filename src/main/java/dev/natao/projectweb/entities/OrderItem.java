def130f84139296ff7a7491955a4b990ba8c5247
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-10 16:51:08 -0300

    mapping entity

--- src/main/java/dev/natao/projectweb/entities/OrderItem.java
+++ src/main/java/dev/natao/projectweb/entities/OrderItem.java
@@ -4,10 +4,16 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import dev.natao.projectweb.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@EmbeddedId // For compost ID
 	private OrderItemPK id;
 	
 	private Integer quantity;
