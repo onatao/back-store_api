8bd9ac5b3da3f8581c8cdb5619474e2d1b0b4897
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-10 17:16:40 -0300

    OrderItemPK null id fixed

--- src/main/java/dev/natao/projectweb/repositories/OrderItemRepository.java
+++ src/main/java/dev/natao/projectweb/repositories/OrderItemRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import dev.natao.projectweb.entities.OrderItem;
+import dev.natao.projectweb.entities.pk.OrderItemPK;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
