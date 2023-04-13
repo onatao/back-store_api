04d334711710845330084058504fdef87a9ff26b
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-13 14:01:22 -0300

    UserResource - URI created

--- src/main/java/dev/natao/projectweb/resources/UserResource.java
+++ src/main/java/dev/natao/projectweb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package dev.natao.projectweb.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import dev.natao.projectweb.entities.User;
 import dev.natao.projectweb.services.UserService;
@@ -37,6 +39,14 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
-		return new ResponseEntity<>(obj, HttpStatus.CREATED);
+		/**
+		 *  Return 201 status and the path where
+		 *  the new user was inserted
+		 */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .path("/{id}")
+				  .buildAndExpand(obj.getUserId())
+				  .toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
