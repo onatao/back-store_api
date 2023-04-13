1d849807ef5ed4d4fa450dbd7f325f18c41b872f
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-13 13:05:52 -0300

    database seeding - payment

--- src/main/java/dev/natao/projectweb/config/TestConfig.java
+++ src/main/java/dev/natao/projectweb/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import dev.natao.projectweb.entities.Category;
 import dev.natao.projectweb.entities.Order;
 import dev.natao.projectweb.entities.OrderItem;
+import dev.natao.projectweb.entities.Payment;
 import dev.natao.projectweb.entities.Product;
 import dev.natao.projectweb.entities.User;
 import dev.natao.projectweb.entities.enums.OrderStatus;
@@ -85,5 +86,8 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 }
