61ecab8eaec8dc960229589b2c498d1c920d0f14
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-13 12:13:47 -0300

    items set created

--- src/main/java/dev/natao/projectweb/entities/Product.java
+++ src/main/java/dev/natao/projectweb/entities/Product.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +33,9 @@ public class Product implements Serializable {
 	joinColumns = @JoinColumn(name = "product_id"), // Product foreign key
 	inverseJoinColumns = @JoinColumn(name = "category_id")) // Category foreign key
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {}
 
@@ -89,6 +93,14 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x: items) {
+			set.add(x.gerOrder());
+		}
+		return set; 
 	}
 
 	@Override
