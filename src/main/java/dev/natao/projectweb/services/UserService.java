da64eb124d94d30b951eb426460ef4e4579563e2
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-15 11:23:16 -0300

    throwing exception on findById - UserService

--- src/main/java/dev/natao/projectweb/services/UserService.java
+++ src/main/java/dev/natao/projectweb/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import dev.natao.projectweb.entities.User;
 import dev.natao.projectweb.repositories.UserRepository;
+import dev.natao.projectweb.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,8 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> userOpt = repository.findById(id);
-		return userOpt.get();
+		// Lambda expression (will return ResourceNotFoundException)
+		return userOpt.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
