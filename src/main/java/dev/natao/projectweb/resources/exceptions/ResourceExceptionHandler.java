ab55a8b7af1dd1ab4e2afab252842ee68615b85c
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-15 11:48:23 -0300

    created DatabaseException handler

--- src/main/java/dev/natao/projectweb/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/dev/natao/projectweb/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import dev.natao.projectweb.services.exceptions.DatabaseException;
 import dev.natao.projectweb.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -25,6 +26,15 @@ public class ResourceExceptionHandler {
 				error, 
 				exception.getMessage(), 
 				request.getRequestURI()); // will return request path	
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException (DatabaseException exception, HttpServletRequest request) {
+		String error = "Database violation";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandardError err = new StandardError(Instant.now(), status.value(), error, exception.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 }
